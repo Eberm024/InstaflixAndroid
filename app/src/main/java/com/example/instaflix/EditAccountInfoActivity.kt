0187fe4ad07b65c923a85facdd491bddad5598f6
Eberm024 <eberm024@fiu.edu>
2022-02-23 16:30:18 -0500

    created activity_edit_account_info.xml, and any other frontend components

---
+++ app/src/main/java/com/example/instaflix/EditAccountInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.instaflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAccountInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_account_info)
+    }
+}
